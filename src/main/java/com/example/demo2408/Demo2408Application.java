431737dcd750930da5d4cd141737367495c03580
thaoutpham <dongxuanhien65.dx@gmail.com>
2021-08-24 16:12:58 +0700

    hello

---
+++ src/main/java/com/example/demo2408/Demo2408Application.java
@@ -0,0 +1,13 @@
+package com.example.demo2408;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo2408Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo2408Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo2408/Demo2408ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo2408;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo2408ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
